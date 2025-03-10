d27540374ed54e83173e8ff01f029e03be9eac7a
PSamu23 <samuelespinal90@gmail.com>
2025-03-09 19:20:46 -0600

    feature:arquitectura inicial

---
+++ Backend/Infraestructure/Persistence/BackendDBContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infraestructure.Persistence
+{
+    public class BackendDBContext : DbContext
+    {
+        public BackendDBContext(DbContextOptions<BackendDBContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
